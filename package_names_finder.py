bab11e671c6c5f3d8773409ff80189b92a966cb9
VerosArea <54151631+CottonCandyHeart@users.noreply.github.com>
2021-05-07 15:24:39 +0200

    Add files via upload

---
+++ package_names_finder.py
@@ -0,0 +1,21 @@
+filepath = "C:\maven\spring-petclinic\dependency_tree.txt"
+tab = [] #later move data to set
+
+try:
+    file = open(filepath, "r")
+    for line in file.readlines():
+        tab.append(line.strip())
+except FileNotFoundError:
+    print("Can't find a file")
+
+index = 0
+for line in tab:
+    tab[index] = tab[index].lstrip("[INFO]")
+    tab[index] = tab[index].strip()
+    tab[index] = tab[index].strip( )
+
+
+    print(tab[index])
+    index+=1
+
+file.close()
