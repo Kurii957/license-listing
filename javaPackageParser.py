9a9eeebfcc9a045432c987c32e63ea0e3648019a
CottonCandyHeart <weronika.bucewka@wp.pl>
2021-05-21 13:09:54 +0200

    Fixing mistakes

--- javaPackageParser.py
+++ javaPackageParser.py
@@ -58,7 +58,7 @@ class JavaPackageParser:
         try:
             to_csv = open(self.output, "w")
             # keys = []
-            to_csv.write("Package name" + sep + "Package version" + sep + "Package name prefix" + sep + "License type" + sep + "Declared License" + sep + "Package URL" + sep + "License URL\n")
+            to_csv.write("Package name" + sep + "Package version" + sep + "Package name prefix" + sep + "License type" + sep + "Declared License\n")
             """
             for key in no_duplicates.keys():
                 keys.append(key)
